df26902577b8111be5d84cef002091567969388c
kevin.cifuentes <kevin.cifuentes@eventbrite.com>
2023-06-18 22:14:19 +0200

    [Delete_all_offers] Added unit and integration tests for DeleteAll case

---
+++ src/test/java/com/inditex/hiring/application/offer/DeleteAllOffersIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.inditex.hiring.application.offer;
+
+import com.inditex.hiring.infrastructure.Application;
+import com.inditex.hiring.infrastructure.persistence.jpa.write.OfferJPARepository;
+import com.inditex.hiring.objectmother.infrastructure.persistence.jpa.read.model.OfferEntityMother;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class DeleteAllOffersIntegrationTest {
+
+    @Autowired
+    private DeleteAllOffersHandler testSubject;
+
+    @Autowired
+    private OfferJPARepository offerJPARepository;
+
+    @After
+    public void tearDown() {
+        offerJPARepository.deleteAll();
+    }
+
+    @Test
+    public void shouldGetOffer() {
+        // given
+        final var offerEntity = OfferEntityMother.random();
+        offerJPARepository.save(offerEntity);
+        final var entityList = offerJPARepository.findAll();
+        assertThat(entityList).isNotEmpty();
+
+        //when
+        testSubject.execute(new DeleteAllOffersCommand());
+
+        //then
+        final var afterEntityList = offerJPARepository.findAll();
+        assertThat(afterEntityList).isEmpty();
+    }
+}
