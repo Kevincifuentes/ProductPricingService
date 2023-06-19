9f5a8e531c35911a3682b9e0fe3568f1c174121a
kevin.cifuentes <kevin.cifuentes@eventbrite.com>
2023-06-19 12:30:29 +0200

    [Find_timetable] Refactored AddOfferRequest mother to reuse code

--- src/test/java/com/inditex/hiring/e2e/offer/models/request/AddOfferRequest.java
+++ src/test/java/com/inditex/hiring/e2e/offer/models/request/AddOfferRequest.java
@@ -1,5 +1,15 @@
 package com.inditex.hiring.e2e.offer.models.request;
 
-public record AddOfferRequest(long offerId, int brandId, long priceListId, String startDate,
-                              String endDate, int priority, String partNumber, double price, String currencyISO) {
+import com.inditex.hiring.infrastructure.controller.dto.Offer;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+
+@Builder(toBuilder = true)
+public record AddOfferRequest(long offerId, int brandId, long priceListId, String startDate, String endDate,
+                              int priority, String partNumber, BigDecimal price, String currencyISO) {
+
+    public Offer toOffer() {
+        return new Offer(offerId, brandId, startDate, endDate, priceListId, partNumber, priority, price, currencyISO);
+    }
 }
